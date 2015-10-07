081587dce10c2eaa711951ec617dd4c2d93711f9
Firedark <firedarknes@gmail.com>
2015-10-07 18:20:45 +0200

    Initial commit

---
+++ android/src/com/gamecell/starcraft/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.gamecell.starcraft.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.gamecell.starcraft.StartCraft;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new StartCraft(), config);
+	}
+}
---
+++ desktop/src/com/gamecell/starcraft/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gamecell.starcraft.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gamecell.starcraft.StartCraft;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StartCraft(), config);
+	}
+}
